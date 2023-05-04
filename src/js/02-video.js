6ff70cb830e6c5ddb01992f80ab08877fa8c883c
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-05-04 22:06:17 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,32 @@
-import Vimeo from '@vimeo/player';
-
+import Player from '@vimeo/player';
+import 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', function (data) {
-  console.log(data);
-});
+const onPlay = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', onPlay);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
