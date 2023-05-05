91ce1b09991488a25524d5e15f8d069d40476682
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-05-05 17:01:58 +0200

    4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,45 @@
+import 'lodash.throttle';
+
 const emailInput = document.querySelector('input');
 const messageTextarea = document.querySelector('textarea');
 const form = document.querySelector('.feedback-form');
 let inputsObject = { email: '', message: '' };
 
-// messageTextarea.value = 'foki';
-
 function currentInput(event) {
-  if (event.currentTarget.type === 'email') {
-    inputsObject.email = event.currentTarget.value;
+  if (event.target.type === 'email') {
+    inputsObject.email = event.target.value;
     console.log(inputsObject);
-  } else if (event.currentTarget.name === 'message') {
-    inputsObject.message = event.currentTarget.value;
+  } else if (event.target.name === 'message') {
+    inputsObject.message = event.target.value;
     console.log(inputsObject);
   }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputsObject));
 }
 
-messageTextarea.addEventListener('input', currentInput);
-emailInput.addEventListener('input', currentInput);
+function updateInputs() {
+  const savedInputs = localStorage.getItem('feedback-form-state');
+  console.log(savedInputs);
+  const parsedInputs = JSON.parse(savedInputs);
+  console.log(parsedInputs);
+
+  emailInput.value = parsedInputs.email;
+  messageTextarea.textContent = parsedInputs.message;
+}
+
+updateInputs();
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log(email.value, message.value);
+
+  localStorage.clear();
+  emailInput.value = '';
+  messageTextarea.textContent = '';
+}
+
+form.addEventListener('input', currentInput);
+form.addEventListener('submit', onSubmit);
