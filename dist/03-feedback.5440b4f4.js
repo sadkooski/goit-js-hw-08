d744f4f987f40bb065f521b99ae699a7c025f37f
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-05-05 13:49:59 +0200

    3

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -557,6 +557,25 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"eH52W":[function(require,module,exports) {
+const emailInput = document.querySelector("input");
+const messageTextarea = document.querySelector("textarea");
+const form = document.querySelector(".feedback-form");
+let inputsObject = {
+    email: "",
+    message: ""
+};
+// messageTextarea.value = 'foki';
+function currentInput(event) {
+    if (event.currentTarget.type === "email") {
+        inputsObject.email = event.currentTarget.value;
+        console.log(inputsObject);
+    } else if (event.currentTarget.name === "message") {
+        inputsObject.message = event.currentTarget.value;
+        console.log(inputsObject);
+    }
+}
+messageTextarea.addEventListener("input", currentInput);
+emailInput.addEventListener("input", currentInput);
 
 },{}]},["jxvN3","eH52W"], "eH52W", "parcelRequire4c75")
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+const emailInput = document.querySelector('input');
+const messageTextarea = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+let inputsObject = { email: '', message: '' };
 
+// messageTextarea.value = 'foki';
+
+function currentInput(event) {
+  if (event.currentTarget.type === 'email') {
+    inputsObject.email = event.currentTarget.value;
+    console.log(inputsObject);
+  } else if (event.currentTarget.name === 'message') {
+    inputsObject.message = event.currentTarget.value;
+    console.log(inputsObject);
+  }
+}
+
+messageTextarea.addEventListener('input', currentInput);
+emailInput.addEventListener('input', currentInput);
